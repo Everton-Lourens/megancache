22037fe1d46ddc3ee3ebc7a2d1f2dfc7955802ae
Everton Lourens <evertonlour98@gmail.com>
2024-02-16 12:11:07 -0300

    update redis

--- cache.js
+++ cache.js
@@ -1,49 +1,35 @@
 const memoryCache = require('memory-cache');
 const { store } = require('meganshop/extensions/meganCache/redis');
 
-
-module.exports.addToRedis = async function addToRedis(key, value, expire = 300) {
-    if (typeof key !== 'string' || (!key && key !== 0) || key === null || key === undefined)
-        throw new Error('===> NÃO TEM NADA NA KEY OU NÃO É UMA STRING <===');
-
-}
-
-module.exports.getFromRedis = async function getFromRedis(key) {
-    if (typeof key !== 'string' || (!key && key !== 0) || key === null || key === undefined)
-        throw new Error('===> NÃO TEM NADA NA KEY OU NÃO É UMA STRING <===');
-    return await _cacheRedis.get(`${key}`) || null;
-}
-
-
 // Adiciona um item ao cache/addToLocalCache
 module.exports.addToLocalCache = async function addToLocalCache(key, value, expire = 300) {
-    if (typeof key !== 'string' || (!key && key !== 0) || key === null || key === undefined)
-        throw new Error('===> NÃO TEM NADA NA KEY OU NÃO É UMA STRING <===');
-    if (store !== null) {
-        console.log('ADD REDIS')
-        await _cacheRedis.set(`${key}`, value)
-        await _cacheRedis.expire(`${key}`, expire); // expire in 5 minutes
-        console.log('ADD REDIS')
-        return true;
-    } else {
-        console.log('ADD LOCALCACHE')
-        await memoryCache.put(key, value, expire * 1000);
-        console.log('ADD LOCALCACHE')
+    try {
+        if (typeof key !== 'string' || (!key && key !== 0) || key === null || key === undefined)
+            throw new Error('===> NÃO TEM NADA NA KEY OU NÃO É UMA STRING <===');
+        if (store !== null) {
+            await store.set(`${key}`, value)
+            await store.expire(`${key}`, expire); // expire in 5 minutes
+            return true;
+        } else {
+            await memoryCache.put(key, value, expire * 1000);
+        }
+    } catch (error) {
+        return false;
     }
 }
 
 // Recupera um item do cache
 module.exports.getFromLocalCache = async function getFromLocalCache(key) {
-    if (typeof key !== 'string' || (!key && key !== 0) || key === null || key === undefined)
-        throw new Error('===> NÃO TEM NADA NA KEY OU NÃO É UMA STRING <===');
-    if (store !== null) {
-        console.log('GET REDIS')
-        console.log('GET REDIS')
-        return await _cacheRedis.get(`${key}`) || null;
-    } else {
-        console.log('GET LOCALCACHE')
-        console.log('GET LOCALCACHE')
-        return await memoryCache.get(key);
+    try {
+        if (typeof key !== 'string' || (!key && key !== 0) || key === null || key === undefined)
+            throw new Error('===> NÃO TEM NADA NA KEY OU NÃO É UMA STRING <===');
+        if (store !== null) {
+            return await store.get(`${key}`) || null;
+        } else {
+            return await memoryCache.get(key) || null;
+        }
+    } catch (error) {
+        return null;
     }
 }
 
